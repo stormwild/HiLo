94132c73d6ffa115366fc2f3e448a9a10b2e785f
Alexander Torrijos <artorrijos@gmail.com>
2019-07-19 20:28:46 +0800

    Added play again loop and try resources

--- src/main/java/games/guessing/HiLo.java
+++ src/main/java/games/guessing/HiLo.java
@@ -3,29 +3,41 @@ package games.guessing;
 import java.util.Scanner;
 
 /**
- * Hello world!
+ * HiLo Guessing Game
  *
  */
 public class HiLo {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int num = (int) (Math.random() * 100 + 1);
-        // System.out.println( "Random number: " + num );
+        try (Scanner scan = new Scanner(System.in)) {
+            int guess = 0;
+            String playAgain = "";
 
-        int guess = 0;
-        while (guess != num) {
-            System.out.println("Guess a number between 1 and 100:");
+            do {
+                int num = (int) (Math.random() * 100 + 1);
+                // System.out.println( "Random number: " + num );
 
-            guess = scan.nextInt();
-            System.out.println("You entered " + guess + ".");
+                while (guess != num) {
+                    System.out.println("Guess a number between 1 and 100:");
 
-            if (guess < num) {
-                System.out.println("You're guess is too low. Try again.");
-            } else if (guess > num) {
-                System.out.println("You're guess is too high. Try again.");
-            } else {
-                System.out.println("You're guess is correct. You win!");
-            }
+                    guess = scan.nextInt();
+                    System.out.println("You entered " + guess + ".");
+
+                    if (guess < num) {
+                        System.out.println("You're guess is too low. Try again.");
+                    } else if (guess > num) {
+                        System.out.println("You're guess is too high. Try again.");
+                    } else {
+                        System.out.println("You're guess is correct. You win!");
+                    }
+                }
+
+                System.out.println("Would you like to play again (y/n)?");
+                playAgain = scan.next();
+            } while (playAgain.equalsIgnoreCase("y"));
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
         }
 
     }
